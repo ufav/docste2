61e07c64ea5d853fc27cfc869b0aafd5e6df3546
Brian McFadden <ufav.bpb@gmail.com>
2024-06-09 14:47:29 +0500

    Initial commit with .gitignore

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,12 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:123@localhost:5432/docste"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
